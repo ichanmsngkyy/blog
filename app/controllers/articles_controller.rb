73629533709bfc868a0870b133675a1fea66c1af
ichanmsngkyy <128625300+ichanmsngkyy@users.noreply.github.com>
2025-09-18 11:13:47 +0800

    Add update and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,9 +21,29 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destory
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
   end
-
 end
